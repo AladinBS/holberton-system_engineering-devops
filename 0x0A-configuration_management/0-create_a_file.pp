b6ee8c40b2f1b11cf87db2b95217b9f8b3c84e8e
AladinBS <aladin.bensassi@gmail.com>
2020-03-23 18:41:32 +0000

    msg

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# make a new file
+file { 'holberton':
+    ensure  => file,
+    path    => '/tmp/holberton',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
