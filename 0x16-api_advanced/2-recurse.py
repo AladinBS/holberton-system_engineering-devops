cd6ca59b86441ab697d149413b641abd866491ed
AladinBS <aladin.bensassi@gmail.com>
2020-07-05 17:30:08 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
+""" Function """
 import requests
 import sys
 
 
 def add_title(hot_list, hot_posts):
-    """ Adds item into a list """
+    """ item ""
     if len(hot_posts) == 0:
         return
     hot_list.append(hot_posts[0]['data']['title'])
@@ -17,7 +14,7 @@ def add_title(hot_list, hot_posts):
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ Queries to Reddit API """
+    """ API """
     u_agent = 'Mozilla/5.0'
     headers = {
         'User-Agent': u_agent
